fc03cc664c5ace502950a834e4f61b7ab3f242aa
nikitaToner <x1tle@yandex.ru>
2026-04-28 14:56:57 +0700

    Добавил всё для Flask и успешной работы сайта

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from Backend.database.install_DB import init_db as DB_Start
 from Backend.parse.install_parse import install as Parse_Start
+from Backend.parse.install_parse import get_last_history,get_active_valute
 from flask import Flask, render_template, jsonify
 from Backend.scheduler.init_scheduler import install as Start_Scheduler
 import threading
@@ -16,10 +17,19 @@ Flask_init = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@Flask_init.route('/api/rates/<currency>')
+def api_rates(currency):
+    return jsonify(get_last_history(currency.upper()))
+
+@Flask_init.route('/api/current')
+def api_current():
+    return jsonify(get_active_valute())
+
+
 
 if __name__ == '__main__':
-    DB_Start(os.getenv("database_name"))
-    Parse_Start(os.getenv("database_name"))
+    DB_Start()
+    Parse_Start()
     threading.Thread(target=Start_Scheduler, daemon=True).start()
     print(f"Привет! Название проекта {os.getenv("project_name")}")
     from waitress import serve
