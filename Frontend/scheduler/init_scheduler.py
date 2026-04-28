21bdda5c09da2a8490f2ba11f856e39c80cd34a8
nikitaToner <x1tle@yandex.ru>
2026-04-28 13:34:28 +0700

    Delete Frontend directory

--- Frontend/scheduler/init_scheduler.py
+++
@@ -1,9 +0,0 @@
-import schedule
-from Frontend.parse.install_parse import init as Parse_start
-
-
-
-def install():
-    schedule.every().day.at("00:00").do(Parse_start)
-    while True:
-        schedule.run_pending()
